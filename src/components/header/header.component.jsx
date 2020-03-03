7c61296779f11f29d3938e1b9508e65eaabdb8d8
wahyufahrozi <wahyufahrozi97@gmail.com>
2020-03-04 02:45:40 +0700

    added cart icon and cart dropdown to app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,10 @@ import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase-db";
 import { Link } from "react-router-dom";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { connect } from "react-redux";
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -25,12 +27,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStatetoProps = state => ({
-  currentUser: state.user.currentUser
+const mapStatetoProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStatetoProps)(Header);
--- src/redux/user/user.types.js
+++ src/redux/user/user.types.js
@@ -1,3 +1,4 @@
 export const UserActionTypes = {
   SET_CURRENT_USER: "SET_CURRENT_USER"
 };
+//best practice to make consisten that value is string

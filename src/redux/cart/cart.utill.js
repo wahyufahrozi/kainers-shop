6a6ddab74eade42c382065585cf4848ddfaeef37
wahyufahrozi <wahyufahrozi97@gmail.com>
2020-03-07 01:38:26 +0700

    add additem and remove item to checkout item component

--- src/redux/cart/cart.utill.js
+++ src/redux/cart/cart.utill.js
@@ -12,4 +12,20 @@ export const addItemTocart = (cartItems, cartItemToAdd) => {
     );
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  //when click the item,then the quantity 1
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    //remove the item from checkoutpage when the quantity is 0
+  }
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
